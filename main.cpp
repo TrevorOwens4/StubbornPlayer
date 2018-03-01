4fb0a5a5c4931bc06c94d32fb33e09c1515ce83b
TrevorOwens4 <towens5@mail.csuchico.edu>
2018-03-01 11:42:46 -0800

    Program takes in a number of how many time user wants to play and always chooses door C

--- main.cpp
+++ main.cpp
@@ -6,6 +6,24 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int gamesWon=0;
+  int timesPlayed = 0;
+
+
+  cout << "How many times would you like to play this game?\n";
+  cin >> timesPlayed;
+
+  for (int i = 0; i < timesPlayed; i++) 
+  {
+    game.newGame();
+    game.guessDoorC();
+    game.guessDoorC();
+    if(game.isWinner())
+    {
+        gamesWon++;
+    }
+  }
+cout<< gamesWon << endl;
+
   return 0;
 }
